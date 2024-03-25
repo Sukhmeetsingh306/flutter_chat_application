b26da8d96cfe6ae4e0b90955487f615d041e7c28
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-25 15:50:18 +0530

    created the login method that it connect with the firebase and correct the widget

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -26,25 +26,28 @@ class _AuthScreenState extends State<AuthScreen> {
     }
     _form.currentState!.save();
 
-    if (_isLogin) {
-    } else {
-      try {
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredentials);
+      } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
         print(userCredentials);
-      } on FirebaseAuthException catch (error) {
-        if (error.code == 'email-already-in-use') {}
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-              error.message ?? 'Authentication Failed',
-            ),
-          ),
-        );
       }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'email-already-in-use') {}
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            error.message ?? 'Authentication Failed',
+          ),
+        ),
+      );
     }
   }
 
@@ -122,7 +125,7 @@ class _AuthScreenState extends State<AuthScreen> {
                                     .colorScheme
                                     .primaryContainer),
                             child: Text(
-                              _isLogin ? 'Sign Up' : 'Login In',
+                              _isLogin ? 'Login In' : 'Sign Up',
                             ),
                           ),
                           TextButton(
