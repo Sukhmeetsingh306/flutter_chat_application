9be32fa193788cfc62017e7705ad3626c205e8cc
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-25 17:11:32 +0530

    made some kind of cookie to be stored in the device so the user don't have enter again and again

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat_application/screen/chat_screen.dart';
 
 import 'firebase_options.dart';
 import 'package:flutter_chat_application/screen/auth_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -23,8 +25,18 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen(),
       debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+        /// this code states if the data is already entered it will directed towards chatScreen else the Auth Screen
+        /// As the token is stored in the device
+      ),
     );
   }
 }
