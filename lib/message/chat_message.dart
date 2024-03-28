167f6fb265b758620023d57f2b1e64883e809cc2
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-28 23:01:47 +0530

    showing chat on screen

--- lib/message/chat_message.dart
+++ lib/message/chat_message.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
@@ -5,6 +6,43 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('No message found'),);
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),// order of the chat display
+      builder: (context, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No Message'),
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(
+            child: Text('Something Went Wrong'),
+          );
+        }
+
+        final loadedMessage = chatSnapshot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessage.length,
+          itemBuilder: (context, index) {
+            return Text(
+              loadedMessage[index].data()['text'],
+            );
+          },
+        );
+      },
+    );
   }
 }
