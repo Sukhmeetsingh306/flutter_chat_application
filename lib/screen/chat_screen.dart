b95d029e33bfbbc5b526a58bbc65cfad8657bb96
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-28 22:26:23 +0530

    started implementing the chat

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_chat_application/message/chat_message.dart';
+import 'package:flutter_chat_application/message/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,23 +10,25 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          )
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(child: ChatMessage()),
+            NewMessage(),
+          ],
+        ));
   }
 }
